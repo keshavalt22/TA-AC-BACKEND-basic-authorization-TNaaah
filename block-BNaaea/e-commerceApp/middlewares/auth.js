6f2a238e4982f219c67cf0039039be8d6bf34507
keshavalt22 <keshavkhatri35@gmail.com>
2022-06-29 22:20:16 +0530

    assignment

---
+++ block-BNaaea/e-commerceApp/middlewares/auth.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    loggedInUser: (req, res, next) => {
+        if(req.session && req.session.userId) {
+            next();
+        }else {
+            res.redirect('/users/login');
+        }
+    }
+}
